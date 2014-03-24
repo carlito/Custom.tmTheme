e56ff6b4c2f3845f2df7f4945a5a79a7215cabcf
Carlos <post@carlito.me>
2014-03-24 17:14:15 +0100

    Splitted theme file in parts, added build script.

---
+++ build.rb
@@ -0,0 +1,72 @@
+require "rexml/document" 
+
+
+files = [
+  'head.xml',
+  'global.xml',
+  'comments.xml',
+  'invalid.xml',
+  'basic.xml',
+  'strings.xml',
+  'html.xml',
+  'css.xml',
+  'markup.xml',
+  'php.xml',
+  'diff.xml',
+  'misc.xml',
+  'linter.xml',
+  'foot.xml'
+]
+
+replacements = {
+  '##red'   => '#e74800',
+  '##blue'  => '#0066ff',
+  '##pink'  => '#c800a4',
+  '##green' => '#00bc2c'
+}
+
+variable_prefix = '##'
+
+
+#before_file = '/* {filename} */' + "\n\n"
+success_msg = 'Files merged.'
+fail_msg    = 'Sorry dude, something is wrong.'
+#args        = ARGV;
+
+def merge(files, replacements)
+  
+  output = ''
+  output_file = 'Theme.tmTheme'
+  
+  files.each do|file|
+    
+    #before_file = before_file.gsub('{filename}', "#{file}")
+    file        = File.read("parts/#{file}")
+
+    # Wirft noch Fehler
+    # replacements.each do|key, value|
+    #   output = output + file.gsub(key, value)
+    # end
+    
+    output = output + file + "\n\n"
+
+  end
+
+
+  # Format XML
+  formatted_output = REXML::Document.new(output)
+  formatter = REXML::Formatters::Pretty.new
+  formatter.compact = true
+  #formatter.write(formatted_output, $stdout)
+  
+
+  File.open(output_file,'w') do |output_file|
+    output_file.puts formatted_output 
+  end
+
+
+return true
+
+end
+
+puts merge(files, replacements) ? success_msg : fail_msg;
